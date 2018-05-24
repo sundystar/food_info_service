4c06cff74d469b5d2937bd87b4dde544b54d6cd4
sh01376 <sh01376@Shenma1372NB.shenmajr.com>
2018-05-24 15:31:02 +0800

    xiugai

---
+++ src/main/java/com/nayouliang/foodinfo/service/FoodInfoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.nayouliang.foodinfo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.foodinfo.facade.FoodInfoFacade;
+
+@Service
+public class FoodInfoServiceImpl implements FoodInfoFacade {
+
+	
+	
+}
